48e20fb83d07bfcdae5a011ea313f4d762a25d87
ayushsupekar <ayushsupekar531@gmail.com>
2025-12-17 02:47:19 -0800

    updated

--- My List/ContentView.swift
+++ My List/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  My List
+//  GroceryList
 //
-//  Created by student on 17/12/25.
+//  Created by Teachers on 17/12/25.
 //
 
 import SwiftUI
@@ -11,44 +11,73 @@ import SwiftData
 struct ContentView: View {
     
     @Environment(\.modelContext) private var modelContext
-    
-    
     @Query private var items: [Item]
-    @State private var isAlertShowing: Bool = false
     
     var body: some View {
         NavigationStack{
-            List {
+            List{
                 ForEach(items){ item in
                     Text(item.title)
+                        .swipeActions{
+                            Button(role: .destructive){
+                                withAnimation{
+                                    modelContext.delete(item)
+                                }
+                                
+                            }label:{
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                        .swipeActions(edge: .leading){
+                            Button("Done", systemImage: item.isCompleted == false ? "checkmark.circle": "x.circle"){
+                                item.isCompleted.toggle()
+                            }
+                            .tint(item.isCompleted == false ? .green : .accentColor)
+                        }
                 }
             }
-            .navigationTitle("Grocery Items")
-            toolbar{
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button{
-                        isAlertShowing.toggle()
-                    }label:{
-                        Image(systemName: "Carrot")
-                            .imageScale(.large)
-                        
+            .navigationTitle("Grocery List")
+            .toolbar{
+                if items.isEmpty{
+                    ToolbarItem(placement: .topBarTrailing){
+                        Button{
+                            // nothing
+                        }label: {
+                            Image(systemName: "carrot")
+                        }
                     }
                 }
             }
-            overlay{
+            .overlay{
                 if items.isEmpty{
-                    ContentUnavailableView("Empty cart", systemImage: "Cart.circle",description: Text("Add some item to the shoping"))
+                    ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description: Text("Add some items to the shopping list!"))
                 }
             }
         }
     }
-    
-}
-#Preview("Second List"){
-     ContentView()
 }
 
-#Preview {
+#Preview("Sample Data") {
+    let sampleData: [Item] = [
+        Item(title: "Bakery & Bread", isCompleted: false),
+        Item(title: "Meat & Seafood", isCompleted: true),
+        Item(title: "Cereals", isCompleted: .random()),
+        Item(title: "Pasta & Rice", isCompleted: .random()),
+        Item(title: "Cheese & Eggs", isCompleted: .random())
+    ]
+    
+    let container = try! ModelContainer(for: Item.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    for item in sampleData {
+        container.mainContext.insert(item)
+    }
+    
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("First List") {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
 }
+
